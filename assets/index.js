15f1f903f68a2308a6f62e15353aee564cad8fe7
Ryan <ryangadinga90@gmail.com>
2024-09-27 13:55:27 +0700

    update html, css, & javascript

--- assets/index.js
+++ assets/index.js
@@ -1,50 +1,63 @@
-let book = document.querySelectorAll ('.book')
+// let book = document.querySelectorAll(".book");
 
-for (books of book)
-{
-    books.addEventListener ('mouseover', function (event) {
-        if (event.target.id === "struktur")
-        {
-            event.target.setAttribute("src", "assets/img/stukturData2.jpg")
-            return;
-        }
-        else if (event.target.id === "mind")
-        {
-            event.target.setAttribute("src", "assets/img/mindset2.jpg")
-            return;
-        }
-        else if (event.target.id === "sakti")
-        {
-            event.target.setAttribute("src", "assets/img/bukuSaktiWeb2.jpg")
-            return;
-        }
-        else if (event.target.id === "self")
-        {
-            event.target.setAttribute("src", "assets/img/selfHealing2.jpg")
-            return;
-        }
-    });
+// for (books of book) {
+//   books.addEventListener("mouseover", function (event) {
+//     if (event.target.id === "struktur") {
+//       event.target.setAttribute("src", "assets/img/stukturData2.jpg");
+//       return;
+//     } else if (event.target.id === "mind") {
+//       event.target.setAttribute("src", "assets/img/mindset2.jpg");
+//       return;
+//     } else if (event.target.id === "sakti") {
+//       event.target.setAttribute("src", "assets/img/bukuSaktiWeb2.jpg");
+//       return;
+//     } else if (event.target.id === "self") {
+//       event.target.setAttribute("src", "assets/img/selfHealing2.jpg");
+//       return;
+//     }
+//   });
 
-    books.addEventListener ('mouseout', function (event){
-        if (event.target.id === "struktur")
-        {
-            event.target.setAttribute("src", "assets/img/stukturData.jpg")
-            return;
-        }
-        else if (event.target.id === "mind")
-        {
-            event.target.setAttribute("src", "assets/img/mindset.jpg")
-            return;
-        }
-        else if (event.target.id === "sakti")
-        {
-            event.target.setAttribute("src", "assets/img/bukuSaktiWeb.jpg")
-            return;
-        }
-        else if (event.target.id === "self")
-        {
-            event.target.setAttribute("src", "assets/img/selfHealing.jpg")
-            return;
-        }
-    });
+//   books.addEventListener("mouseout", function (event) {
+//     if (event.target.id === "struktur") {
+//       event.target.setAttribute("src", "assets/img/stukturData.jpg");
+//       return;
+//     } else if (event.target.id === "mind") {
+//       event.target.setAttribute("src", "assets/img/mindset.jpg");
+//       return;
+//     } else if (event.target.id === "sakti") {
+//       event.target.setAttribute("src", "assets/img/bukuSaktiWeb.jpg");
+//       return;
+//     } else if (event.target.id === "self") {
+//       event.target.setAttribute("src", "assets/img/selfHealing.jpg");
+//       return;
+//     }
+//   });
+// }
+
+let book = document.querySelectorAll(".book");
+
+for (books of book) {
+  books.addEventListener("mouseover", function (event) {
+    if (event.target.id === "struktur") {
+      event.target.setAttribute("src", "assets/img/stukturData2.jpg");
+    } else if (event.target.id === "mind") {
+      event.target.setAttribute("src", "assets/img/mindset2.jpg");
+    } else if (event.target.id === "sakti") {
+      event.target.setAttribute("src", "assets/img/bukuSaktiWeb2.jpg");
+    } else if (event.target.id === "self") {
+      event.target.setAttribute("src", "assets/img/selfHealing2.jpg");
+    }
+  });
+
+  books.addEventListener("mouseout", function (event) {
+    if (event.target.id === "struktur") {
+      event.target.setAttribute("src", "assets/img/stukturData.jpg");
+    } else if (event.target.id === "mind") {
+      event.target.setAttribute("src", "assets/img/mindset.jpg");
+    } else if (event.target.id === "sakti") {
+      event.target.setAttribute("src", "assets/img/bukuSaktiWeb.jpg");
+    } else if (event.target.id === "self") {
+      event.target.setAttribute("src", "assets/img/selfHealing.jpg");
+    }
+  });
 }
